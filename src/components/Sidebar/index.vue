f5d563b3f0cc9f94f2ce23de8d1a676f41bf469a
Trim21 <trim21me@gmail.com>
2017-10-09 23:03:09 +0800

    nothing

--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -1,60 +1,52 @@
 <template>
-  <md-whiteframe md-theme="white" md-tag="div" class="sidebar" data-color="blue">
-    <a href="/" target="_self">
-      <div class="logo">
+  <div class="sidebar" data-color="blue">
+    <div class="logo">
+      <a href="/" target="_self">
         <img class="center-block img-circle" src="/static/images/logo.jpg" alt="/">
-      </div>
-    </a>
+      </a>
+    </div>
     <div class="sidebar-wrapper">
       <ul class="nav">
         <item title="Bangumi" icon="video_library" to="/"></item>
         <item title="Calendar" icon="event" to="/calendar"></item>
         <item title="Resource" icon="rss_feed" to="/resource"></item>
-        <!--<item title="Search" icon="search" to="/search"></item>-->
-        <!--<item title="Config" icon="settings" to="/config"></item>-->
         <li>
           <hr>
         </li>
-        <!--<item title="Admin" icon="settings" to="/admin" @click></item>-->
         <item title="Subscribe" icon="subscriptions" to="/subscribe"></item>
-        <!--<item title="Feed" icon="rss_feed" href="/resource/feed.xml"></item>-->
-
         <li>
           <hr>
         </li>
         <item title="About" icon="help_outline" to="/about"></item>
-        <!--<item title="Bug Report" icon="report" href="https://github.com/BGmi/BGmi/issues"></item>-->
         <item title="Based On BGmi" href="https://github.com/BGmi/BGmi"></item>
       </ul>
     </div>
     <div class="sidebar-background"></div>
-  </md-whiteframe>
+  </div>
 </template>
-<script>
-  import Item from './item'
 
-  export default {
-    name: 'sidebar',
-    props: {
-      open: Boolean
-    },
-    methods: {
-      expand () {
-        console.log('expand')
-      }
-    },
-    components: {
-      Item
-    }
+<script>
+import Item from './item'
+
+export default {
+  name: 'sidebar',
+  props: {
+    open: Boolean
+  },
+  methods: {
+  },
+  components: {
+    Item
   }
+}
 </script>
 
 <style type="text/scss" lang="scss" rel="stylesheet/scss">
-  .sidebar-background {
-    background-image: url('~images/sidebar-2.jpg')
-  }
+.sidebar-background {
+  background-image: url('~images/sidebar-2.jpg')
+}
 
-  a {
-    text-decoration: none
-  }
+a {
+  text-decoration: none
+}
 </style>
