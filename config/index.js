02e86fa82d94a98131f46c6375caa289cb5c9abe
Trim21 <trim21me@gmail.com>
2018-01-29 17:08:33 +0800

    fix(subscribe): mark script
    script bangumi card can expand and mark now

--- config/index.js
+++ config/index.js
@@ -27,7 +27,22 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api',
+        }
+      },
+      '/bangumi/': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/': '/'
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
--- src/pages/Subscribe/bangumiCard.vue
+++ src/pages/Subscribe/bangumiCard.vue
@@ -29,60 +29,60 @@
         pack
       </md-button>
       <div v-if="expand">
-        <div class="row">
-          <div class="col-md-12" v-cloak>
-            <md-input-container>
-              <label>Status</label>
-              <md-input type="number" v-model="bangumi.status"></md-input>
-            </md-input-container>
+        <div class="row"
+        <div class="col-md-12" v-cloak>
+          <md-input-container v-if="!script">
+            <label>Status</label>
+            <md-input type="number" v-model="bangumi.status"/>
+          </md-input-container>
 
-            <md-input-container md-clearable>
-              <label>Include</label>
-              <md-input v-model="filter.include"></md-input>
-            </md-input-container>
+          <md-input-container md-clearable v-if="!script">
+            <label>Include</label>
+            <md-input v-model="filter.include"/>
+          </md-input-container>
 
-            <md-input-container md-clearable>
-              <label>Regex</label>
-              <md-input v-model="filter.regex"></md-input>
-            </md-input-container>
+          <md-input-container md-clearable v-if="!script">
+            <label>Regex</label>
+            <md-input v-model="filter.regex"/>
+          </md-input-container>
 
-            <md-input-container md-clearable>
-              <label>Exclude</label>
-              <md-input v-model="filter.exclude"></md-input>
-            </md-input-container>
+          <md-input-container md-clearable v-if="!script">
+            <label>Exclude</label>
+            <md-input v-model="filter.exclude"/>
+          </md-input-container>
 
-            <md-input-container>
-              <label>Episode</label>
-              <md-input type="number" v-model="mark"></md-input>
-            </md-input-container>
+          <md-input-container>
+            <label>Episode</label>
+            <md-input type="number" v-model="mark"/>
+          </md-input-container>
 
-            <md-checkbox
-              v-for="( key,value) in filter.subtitle_group"
-              v-model="followed"
-              :key="key"
-              :md-value="key">
-              {{key}}
-            </md-checkbox>
-          </div>
-          <md-button class="md-raised md-accent" @click="del()">
-            delete
-          </md-button>
-          <md-button class="md-raised" @click="save()">
-            save
-          </md-button>
+          <md-checkbox
+            v-for="( key,value) in filter.subtitle_group"
+            v-model="followed"
+            :key="key"
+            :md-value="key">
+            {{key}}
+          </md-checkbox>
         </div>
+        <md-button class="md-raised md-accent" @click="del()">
+          delete
+        </md-button>
+        <md-button class="md-raised" @click="save()">
+          save
+        </md-button>
       </div>
-      <br>
     </div>
+    <br>
+  </div>
   </div>
 </template>
 <script>
   let imgRoot = ''
-  if (process.env.NODE_ENV === 'development') {
-    imgRoot = 'http://localhost:8888/bangumi/cover/'
-  } else {
-    imgRoot = '/bangumi/cover/'
-  }
+  // if (process.env.NODE_ENV === 'development') {
+  //   imgRoot = 'http://localhost:8888/bangumi/cover/'
+  // } else {
+  imgRoot = '/bangumi/cover/'
+  // }
 
   export default {
     data () {
@@ -98,6 +98,7 @@
           bangumi_name: 'NEW GAME!!',
           include: null
         },
+        script: false,
         src: '',
         followed: [],
         mark: this.bangumi.episode
@@ -132,8 +133,15 @@
       },
       save () {
         console.log(this.filter_args)
-        Promise.all([this.$http.post('api/filter', this.filter_args),
-          this.$http.post('api/mark', {name: this.bangumi.name, episode: this.mark})])
+        let p = []
+        if (!this.script) {
+          p = [this.$http.post('api/filter', this.filter_args),
+            this.$http.post('api/mark', {name: this.bangumi.name, episode: this.mark})]
+        } else {
+          p = [
+            this.$http.post('api/mark', {name: this.bangumi.name, episode: this.mark})]
+        }
+        Promise.all(p)
           .then(
             res => {
               this.$notifications.notify({
@@ -158,11 +166,16 @@
         this.expand = true
       },
       expandDetail () {
-        this.$http.post('api/filter', {name: this.bangumi.name}).then(
-          res => {
-            this.fetchFilter(res.body.data)
-          }
-        )
+        if (this.bangumi.id) {
+          this.$http.post('api/filter', {name: this.bangumi.name}).then(
+            res => {
+              this.fetchFilter(res.body.data)
+            }
+          )
+        } else {
+          this.expand = true
+          this.script = true
+        }
       },
       add () {
         const action = 'add'
