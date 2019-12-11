abe1b8375aa73b439fc923e87f8e00cf4c36bf9e
Trim21 <i@trim21.me>
2019-12-11 21:55:25 +0800

    update deps

--- src/utils.ts
+++ src/utils.ts
@@ -1,23 +1,10 @@
 import store from './store'
 
-// import * as _ from ''
-
 function hasWatched (bangumiName: string, episode: string) {
-  bangumiName = bangumiName.toString()
-  episode = episode.toString()
   if (Object.prototype.hasOwnProperty.call(store.state.history, bangumiName) &&
     Object.prototype.hasOwnProperty.call(store.state.history[bangumiName], episode)) {
     return store.state.history[bangumiName][episode]
   }
-}
-
-function isEmpty (obj: any) {
-  for (const prop in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, prop)) {
-      return false
-    }
-  }
-  return true
 }
 
 function normalizePath (url: string) {
@@ -27,6 +14,5 @@ function normalizePath (url: string) {
 
 export {
   normalizePath,
-  isEmpty,
   hasWatched
 }
