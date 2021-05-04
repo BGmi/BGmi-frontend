778325c8ebe172fdc34b58aa26e778d32226c581
Trim21 <i@trim21.me>
2021-05-04 08:52:20 +0800

    mtests: basic test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -2,9 +2,11 @@ import { expect } from 'chai';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import About from '@/pages/About.vue';
 import Vuex, { Store } from 'vuex';
+import Vuetify from 'vuetify';
 
 const localVue = createLocalVue();
 
+localVue.use(Vuetify);
 localVue.use(Vuex);
 
 describe('About.vue', () => {
@@ -13,21 +15,19 @@ describe('About.vue', () => {
   beforeEach(() => {
     store = new Vuex.Store({
       state: { bgmiVersion: 'bgmi-version' },
-      actions: {}
+      actions: {},
     });
   });
 
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
     const wrapper = shallowMount(About, {
-      store, localVue
+      store, localVue,
     });
     expect(wrapper.text()).to.include('bgmi-version');
   });
   it('Build Version', () => {
-    const msg = 'new message';
     const wrapper = shallowMount(About, {
-      store, localVue
+      store, localVue,
     });
     expect(wrapper.text()).to.include(process.env.VUE_APP_VERSION);
   });
