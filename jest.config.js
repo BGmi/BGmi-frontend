dba69383afacc93e657f82183f01229537df575a
kahosan <15116355657l@gmail.com>
2022-11-23 18:30:35 +0800

    lint

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.{js,vue,ts}',
-    '!src/main.ts' // No need to cover bootstrap file
-  ]
+    '!src/main.ts', // No need to cover bootstrap file
+  ],
 };
