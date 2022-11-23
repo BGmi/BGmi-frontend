8f4339d14a930ba4bcd9e3ac576481d5f90eca82
Trim21 <trim21.me@gmail.com>
2022-11-23 23:19:23 +0800

    build: upgrade new webpack devServer

--- tests/mock_app.js
+++ tests/mock_app.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 
-module.exports = function setup(app) {
-  app.get('/api/cal', function(req, res) {
+module.exports = function setup(mw, server) {
+  server.app.get('/api/cal', function(req, res) {
     fs.readFile(__dirname + '/mock/cal.json', function(r, data) {
       res.send(data);
     });
   });
+
+  return mw;
 };
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,23 @@
+const { defineConfig } = require('@vue/cli-service');
+
 const pkg = require('./package.json');
 const setup = require('./tests/mock_app');
 
 // 作为配置文件，直接导出配置对象即可
 process.env.VUE_APP_VERSION = pkg.version;
 
-module.exports = {
+module.exports = defineConfig({
   publicPath: './',
   configureWebpack: {
     performance: { hints: false },
-    resolve: { fallback: { 'path': require.resolve('path-browserify') } },
+    resolve: { fallback: { path: require.resolve('path-browserify') } },
   },
   devServer: {
     // 设置主机地址
     host: 'localhost',
     // 设置默认端口
     port: 8080,
-    setup,
+    setupMiddlewares: setup,
     // 设置代理
     proxy: {
       '/api': {
@@ -34,4 +36,4 @@ module.exports = {
       },
     },
   },
-};
+});
