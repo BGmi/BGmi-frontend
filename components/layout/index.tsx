a5f6a0e395b7a7c92d93c2be4705db5427193b8d
kahosan <15116355657l@gmail.com>
2023-03-15 16:44:00 +0800

    style: remove Divider

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { useState } from 'react';
 import Header from '../header';
 import Sidebar from '../sidebar';
@@ -10,7 +10,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Box minH="100vh" ml={{ md: '60' }}>
       <Header sidebarToggle={handleToggle} />
-      <Divider />
       <Sidebar isOpen={open} onClose={handleToggle} />
       <Box as="main" p="4">
         {children}
