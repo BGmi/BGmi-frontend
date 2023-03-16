7398c170f872315d2c623809c1a12366608704df
kahosan <15116355657l@gmail.com>
2023-03-16 15:38:19 +0800

    feat: resource

--- pages/resource/index.tsx
+++ pages/resource/index.tsx
@@ -1,5 +1,13 @@
+import { Box, Link, Stack } from '@chakra-ui/react';
+
 export default function Resource() {
+  // 与原来保持一致
   return (
-    <div>resource</div>
+    <Box>
+      <Stack spacing="2">
+        <Link href="/resource/feed.xml" target="_blank" color="blue.200">Rss Feed</Link>
+        <Link href="/resource/calendar.ics" target="_blank" color="blue.200">ICS Calendar for mobile phone</Link>
+      </Stack>
+    </Box>
   );
 }
