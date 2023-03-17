593086d30197409996935bb4c5eefcc6626ced85
kahosan <15116355657l@gmail.com>
2023-03-17 20:38:54 +0800

    feat: extrace video componet

--- pages/player/[bangumi].tsx
+++ pages/player/[bangumi].tsx
@@ -2,74 +2,18 @@ import { Box, Flex, Heading } from '@chakra-ui/react';
 import Head from 'next/head';
 
 import { useRouter } from 'next/router';
-import { useEffect, useMemo, useRef, useState } from 'react';
-
-import type DPlayer from 'dplayer';
-
-import EpisodeCard from '~/components/episone-card';
+import VideoPlayer from '~/components/video-player';
 
 import type { BangumiData } from '~/hooks/use-bangumi';
 import { useWatchHistory } from '~/hooks/use-watch-history';
-import { useColorMode } from '~/hooks/use-color-mode';
 
 export default function Player() {
   const router = useRouter();
-  const { colorMode } = useColorMode();
   const bangumiData = JSON.parse(router.query.bangumiData as string || '{}') as BangumiData;
 
-  const [watchHistory, setWatchHistory] = useWatchHistory();
+  const [watchHistory] = useWatchHistory();
   // 如果没有观看历史默认选择第一集
   const episone = watchHistory[bangumiData?.bangumi_name]?.['cur-watch'] as number || 1;
-  // 触发 DPlayer 重新加载
-  const [playUrl, setPlayUrl] = useState<string>();
-
-  const containerRef = useRef<HTMLDivElement>(null);
-  const dPlayerRef = useRef<DPlayer>();
-
-  const onDPlay = (url: string, ep: number) => {
-    setPlayUrl(url);
-
-    setWatchHistory({
-      ...watchHistory,
-      [bangumiData.bangumi_name]: {
-        ...watchHistory[bangumiData.bangumi_name],
-        [ep]: 'mark',
-        'cur-watch': ep
-      }
-    });
-  };
-
-  // 传给 Episode Card
-  const playParams = useMemo(() => {
-    if (bangumiData.player) {
-      return {
-        episode: Object.keys(bangumiData.player).map(ep => parseInt(ep, 10)),
-        playUrl: bangumiData.player, // { episode: "path": "/bangumi_file.mp4" }
-        totalMark: watchHistory[bangumiData.bangumi_name] as Record<string, 'mark' | undefined>
-      };
-    }
-  }, [bangumiData.bangumi_name, bangumiData.player, watchHistory]);
-
-  useEffect(() => {
-    // ssr error self is not defined
-    if (bangumiData.player) {
-      setPlayUrl(bangumiData.player?.[episone]?.path);
-      import('dplayer').then((pack) => {
-        const DPlayer = pack.default;
-
-        dPlayerRef.current = new DPlayer({
-          container: containerRef.current,
-          video: {
-            url: playUrl ? `/bangumi/${playUrl}` : ''
-            // TODO 裁剪封面图
-            // pic: playUrl ? bangumiData.cover : ''
-          }
-        });
-      });
-    }
-
-    return () => dPlayerRef.current?.destroy();
-  }, [bangumiData.cover, bangumiData.player, episone, playUrl]);
 
   return (
     <Box>
@@ -80,21 +24,7 @@ export default function Player() {
         {bangumiData.bangumi_name} {episone ? `- 第 ${episone} 集` : ''}
       </Heading>
       <Flex position="relative" mx={{ lg: '30', base: 'unset' }} flexDirection={{ xl: 'row', base: 'column' }}>
-        <Box
-          p="4"
-          rounded="4"
-          bg={colorMode === 'dark' ? 'whiteAlpha.200' : 'blackAlpha.200'}
-          boxShadow="base"
-          transition=".5s width"
-          minH="50%"
-          w="full"
-        >
-          <div
-            id="DPlayer"
-            ref={containerRef}
-        />
-        </Box>
-        <EpisodeCard boxShadow="base" onDPlay={onDPlay} playParams={playParams} />
+        <VideoPlayer bangumiData={bangumiData} episone={episone} />
       </Flex>
     </Box>
   );
