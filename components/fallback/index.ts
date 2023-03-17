2dc1a52405e5d4330532dd90f6836753ea9716c8
kahosan <15116355657l@gmail.com>
2023-03-17 20:57:27 +0800

    fix: ref path

--- components/fallback/index.ts
+++ components/fallback/index.ts
@@ -1,2 +1,5 @@
 export { default as FallbackBangumi } from './bangumi';
+export { default as FallbackEpisodeCard } from './episone-card';
+export { default as FallbackCalendar } from './calendar';
+
 export { default as Skeleton } from './skeleton';
