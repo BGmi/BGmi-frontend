9f6a76604d523555f88507b604988a527193b42e
kahosan <15116355657l@gmail.com>
2023-03-21 22:26:17 +0800

    style: height and weight

--- components/fallback/calendar.tsx
+++ components/fallback/calendar.tsx
@@ -2,7 +2,6 @@ import { Card, CardBody, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/r
 import Skeleton from './skeleton';
 
 export default function FallbackCalendar() {
-  const skeletonTabLists: JSX.Element[] = [];
   const skeletonTabPanels: JSX.Element[] = [];
   const skeletonTabPanelItems: JSX.Element[] = [];
 
@@ -10,7 +9,7 @@ export default function FallbackCalendar() {
     skeletonTabPanelItems.push(
       <Card key={`tab-panel-item-${i}`} maxW="xl">
         <CardBody display="flex">
-          <Skeleton h="44" minW="32" w="32" />
+          <Skeleton h="250px" minW="180px" />
           <Skeleton h="10" w="full" ml="4" />
         </CardBody>
       </Card>
@@ -18,10 +17,6 @@ export default function FallbackCalendar() {
   }
 
   for (let i = 0; i < 6; ++i) {
-    skeletonTabLists.push(
-      <Skeleton w="24" h="10" mx="1" key={`tab=list-${i}`} />
-    );
-
     skeletonTabPanels.push(
       <TabPanel
         display="grid"
@@ -38,7 +33,7 @@ export default function FallbackCalendar() {
   return (
     <Tabs position="relative">
       <TabList top="4px" borderBottom="none" pb="2px">
-        {...skeletonTabLists}
+        <Skeleton w="60%" h="8" mx="1" />
       </TabList>
       <TabPanels>
         {...skeletonTabPanels}
