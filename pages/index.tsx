1a867d187b3f6635f1e5ce3fecdd996b812f9c35
kahosan <15116355657l@gmail.com>
2023-03-21 22:30:35 +0800

    feat: placeholder background and add expand animation to the title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import { Box, Grid, GridItem, Heading, Stack, Text } from '@chakra-ui/react';
+import { Box, Fade, Grid, GridItem, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import { useState } from 'react';
 
 import { FallbackBangumi } from '~/components/fallback';
+import Layout from '~/components/layout';
 import Link from '~/components/router-link';
 
 import type { BangumiData } from '~/hooks/use-bangumi';
@@ -15,6 +17,7 @@ interface PlayerCardProps {
 
 function PlayerCard({ bangumiData }: PlayerCardProps) {
   const { colorMode } = useColorMode();
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   const { bangumi_name: title, cover: coverUrl, episode } = bangumiData;
 
@@ -26,46 +29,47 @@ function PlayerCard({ bangumiData }: PlayerCardProps) {
       roundedBottom="md"
       >
       <Link href={`/player/${title}`} overflow="hidden">
-        <Box
-          h="48"
-          w="full"
-          bg="blackAlpha.300"
-          backgroundImage={BASE_PATH + coverUrl}
-          backgroundRepeat="no-repeat"
-          backgroundSize="cover"
-          backgroundPosition="50% 50%"
-          position="relative"
-          overflow="hidden"
-        >
-          <Box
-            opacity="0"
-            position="absolute"
-            top="0"
-            zIndex="1"
-            w="full"
-            h="full"
-            transform="scale(1.5)"
-            transition="all 0.3s ease"
-            background={'rgba(0,0,0,0.4) url("data:image/svg+xml,%3C%3Fxml version=\'1.0\' encoding=\'utf-8\'%3F%3E%3C!-- Generator: Adobe Illustrator 21.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0) --%3E%3Csvg version=\'1.1\' id=\'图层_1\' xmlns=\'http://www.w3.org/2000/svg\' xmlns:xlink=\'http://www.w3.org/1999/xlink\' x=\'0px\' y=\'0px\' viewBox=\'0 0 50 50\' style=\'enable-background:new 0 0 50 50;\' xml:space=\'preserve\'%3E%3Cstyle type=\'text/css\'%3E .st0%7Bfill:%23FFFFFF;%7D%0A%3C/style%3E%3Cpath class=\'st0\' d=\'M35.4,28L18.4,38.6c-2.4,1.5-5.4-0.2-5.4-3V14.3c0-2.8,3.1-4.5,5.4-3L35.3,22C37.6,23.3,37.6,26.6,35.4,28z\'/%3E%3C/svg%3E%0A") center no-repeat'}
-            backgroundSize="25% 25%"
-            _hover={{
-              opacity: '1',
-              transform: 'scale(1)'
-            }}
-          />
+        <Box bg={colorMode === 'dark' ? 'gray.900' : 'gray.200'} minH="48">
+          <Fade in={imageLoaded}>
+            <Box position="relative" overflow="hidden">
+              <Image
+                h="48"
+                w="full"
+                src={BASE_PATH + coverUrl}
+                alt="anime cover"
+                objectFit="cover"
+                backgroundPosition="50% 50%"
+                onLoad={() => setImageLoaded(true)}
+           />
+              <Box
+                opacity="0"
+                position="absolute"
+                top="0"
+                zIndex="1"
+                w="full"
+                h="full"
+                transform="scale(1.5)"
+                transition="all 0.3s ease"
+                background={'rgba(0,0,0,0.4) url("data:image/svg+xml,%3C%3Fxml version=\'1.0\' encoding=\'utf-8\'%3F%3E%3C!-- Generator: Adobe Illustrator 21.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0) --%3E%3Csvg version=\'1.1\' id=\'图层_1\' xmlns=\'http://www.w3.org/2000/svg\' xmlns:xlink=\'http://www.w3.org/1999/xlink\' x=\'0px\' y=\'0px\' viewBox=\'0 0 50 50\' style=\'enable-background:new 0 0 50 50;\' xml:space=\'preserve\'%3E%3Cstyle type=\'text/css\'%3E .st0%7Bfill:%23FFFFFF;%7D%0A%3C/style%3E%3Cpath class=\'st0\' d=\'M35.4,28L18.4,38.6c-2.4,1.5-5.4-0.2-5.4-3V14.3c0-2.8,3.1-4.5,5.4-3L35.3,22C37.6,23.3,37.6,26.6,35.4,28z\'/%3E%3C/svg%3E%0A") center no-repeat'}
+                backgroundSize="25% 25%"
+                _hover={{
+                  opacity: '1',
+                  transform: 'scale(1)'
+                }}
+            />
+            </Box>
+          </Fade>
         </Box>
       </Link>
       <Stack minH="5.5rem" p="4" bg={colorMode === 'light' ? 'blackAlpha.200' : 'whiteAlpha.100'} position="relative">
         <Heading
-          whiteSpace="nowrap"
+          maxH="6"
           overflow="hidden"
-          textOverflow="ellipsis"
           fontSize="xl"
           fontFamily="body"
+          transition="max-height 0.3s ease"
           _hover={{
-            whiteSpace: 'unset',
-            overflow: 'unset',
-            textOverflow: 'unset'
+            maxH: '16'
           }}
         >
           {title}
@@ -96,3 +100,11 @@ export default function Bangumi() {
     </Grid>
   );
 }
+
+Bangumi.getLayout = function getLayout(page: React.ReactElement) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  );
+};
