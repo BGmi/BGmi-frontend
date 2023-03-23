a186b4907353bf5ef3bb2f2a5406a39bb5686bca
kahosan <15116355657l@gmail.com>
2023-03-23 10:47:38 +0800

    feat: timeout handler

--- hooks/use-auth.ts
+++ hooks/use-auth.ts
@@ -3,12 +3,19 @@ import { BASE_PATH } from '~/lib/contant';
 
 export const useAuth = () => {
   const tryAuth = async (authToken: string) => {
+    // request timeout
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => {
+      controller.abort();
+    }, 3000);
+
     const options: RequestInit = {
+      signal: controller.signal,
       method: 'POST',
       body: JSON.stringify({ token: authToken })
     };
 
-    return await fetch(`${BASE_PATH}/api/auth`, options);
+    return { timeoutId, response: await fetch(`${BASE_PATH}/api/auth`, options) };
   };
 
   return {
