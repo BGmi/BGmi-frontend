da9f2ab3bb17f3e766faf210bb98e9ed3422e97c
kahosan <15116355657l@gmail.com>
2023-03-29 20:34:39 +0800

    feat: document title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
-import { Outlet } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
+import { Outlet, useLocation } from 'react-router-dom';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { theme } from '~/lib/chakra-theme';
 import Layout from '~/components/layout';
+import { handleSecondaryTitle } from '~/lib/utils';
 
 export default function App() {
+  const { pathname } = useLocation();
+  const headTitle = `BGmi - ${pathname === '/' ? 'Bangumi' : handleSecondaryTitle(pathname) ?? ''}`;
+
   return (
     <HelmetProvider>
+      <Helmet>
+        <title>{headTitle}</title>
+      </Helmet>
       <ChakraProvider theme={theme}>
         <Layout>
           <Outlet />
