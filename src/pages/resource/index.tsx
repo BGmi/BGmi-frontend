422cc95346f2af47f2479e22a72eb70152e1b8b6
kahosan <15116355657l@gmail.com>
2023-03-30 13:09:08 +0800

    fix: resource path

--- src/pages/resource/index.tsx
+++ src/pages/resource/index.tsx
@@ -1,13 +1,11 @@
-import { Box, Stack } from '@chakra-ui/react';
-
-import Link from '~/components/router-link';
+import { Box, Stack, Link } from '@chakra-ui/react';
 
 export default function Resource() {
   return (
     <Box>
       <Stack spacing="2">
-        <Link href="/resource/feed.xml" target="_blank" color="blue.200">Rss Feed</Link>
-        <Link href="/resource/calendar.ics" target="_blank" color="blue.200">ICS Calendar for mobile phone</Link>
+        <Link href="./resource/feed.xml" target="_blank" color="blue.200">Rss Feed</Link>
+        <Link href="./resource/calendar.ics" target="_blank" color="blue.200">ICS Calendar for mobile phone</Link>
       </Stack>
     </Box>
   );
