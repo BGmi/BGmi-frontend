eea954649ce9b6db9d776a2d21cc1d04d6fff99f
Trim21 <trim21.me@gmail.com>
2023-03-31 20:00:19 +0800

    chore: add lint-staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,18 @@
 module.exports = {
-  "root": true,
-  "ignorePatterns": [
-    "dist/**"
-  ],
-  "overrides": [
+  root: true,
+  ignorePatterns: ['dist/**'],
+  overrides: [
     {
-      "rules": {
-        "@fluffyfox/jsx/no-template-literal": "off",
-        "@typescript-eslint/object-curly-spacing": ["error", "always"]
+      rules: {
+        '@fluffyfox/jsx/no-template-literal': 'off',
+        '@typescript-eslint/object-curly-spacing': ['error', 'always'],
       },
-      "files": [
-        "**/*.d.ts",
-        "**/*.ts",
-        "**/*.tsx"
-      ],
-      "parserOptions": {
-        "project": "./tsconfig.json",
-        "sourceType": "module"
+      files: ['**/*.d.ts', '**/*.ts', '**/*.tsx'],
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
       },
-      "extends": [
-        "kaho/react",
-        "kaho/ts",
-        "plugin:prettier/recommended"
-      ]
-    }
-  ]
-}
+      extends: ['kaho/react', 'kaho/ts', 'plugin:prettier/recommended'],
+    },
+  ],
+};
