62dd9220379bf8f5b0867e0937d7a1f9f903ba27
kaho <15116355657l@gmail.com>
2023-04-13 18:35:26 +0800

    feat: improve calendar and subscribe (#225)

--- src/components/calendar-tab/index.tsx
+++ src/components/calendar-tab/index.tsx
@@ -17,14 +17,32 @@ export default function CalendarTab({
   boxProps,
   ...props
 }: Props & TabsProps) {
+  const engToZh: Record<string, string> = {
+    mon: '周一',
+    tue: '周二',
+    wed: '周三',
+    thu: '周四',
+    fri: '周五',
+    sat: '周六',
+    sun: '周日',
+  };
+
+  const keys = Object.keys(engToZh);
+
+  // eslint-disable-next-line @fluffyfox/no-unsafe-date -- ignore
+  const date = new Date().getDay();
+  const toDay = date !== 0 ? date - 1 : 6;
+
   return (
-    <Tabs position="relative" isLazy lazyBehavior="keepMounted" {...props}>
+    <Tabs position="relative" isLazy lazyBehavior="keepMounted" {...props} defaultIndex={toDay}>
       <TabList top="4px" borderBottom="none" pb="2px" minH="42px" {...tabListProps}>
-        {tabListItems.map(week => (
-          <Tab mb="-2px" key={week}>
-            {week[0].toUpperCase() + week.slice(1)}
-          </Tab>
-        ))}
+        {tabListItems
+          .sort((a, b) => (keys.indexOf(a) > keys.indexOf(b) ? 1 : -1))
+          .map(week => (
+            <Tab whiteSpace="nowrap" mb="-2px" key={week}>
+              {engToZh[week]}
+            </Tab>
+          ))}
       </TabList>
       <Box top="46.5px" borderBottom="2px solid" mt="-2.5px" borderBottomColor="inherit" {...boxProps} />
 
