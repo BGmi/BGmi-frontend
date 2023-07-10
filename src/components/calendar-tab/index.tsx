48442cbb1951b380bbd9396a0c9b75d206d45b0c
kaho <15116355657l@gmail.com>
2023-07-10 09:25:32 +0800

    fix: misalignment between tab and panel in `calendar-tab` (#231)

--- src/components/calendar-tab/index.tsx
+++ src/components/calendar-tab/index.tsx
@@ -29,22 +29,17 @@ export default function CalendarTab({
     unknown: '未知',
   };
 
-  const keys = Object.keys(engToZh);
-
   // eslint-disable-next-line @fluffyfox/no-unsafe-date -- ignore
-  const date = new Date().getDay();
-  const toDay = date !== 0 ? date - 1 : 6;
+  const toDay = new Date().getDay();
 
   return (
     <Tabs position="relative" isLazy lazyBehavior="keepMounted" {...props} defaultIndex={toDay}>
       <TabList top="4px" borderBottom="none" pb="2px" minH="42px" {...tabListProps}>
-        {tabListItems
-          .sort((a, b) => (keys.indexOf(a) > keys.indexOf(b) ? 1 : -1))
-          .map(week => (
-            <Tab whiteSpace="nowrap" mb="-2px" key={week}>
-              {engToZh[week]}
-            </Tab>
-          ))}
+        {tabListItems.map(week => (
+          <Tab whiteSpace="nowrap" mb="-2px" key={week}>
+            {engToZh[week]}
+          </Tab>
+        ))}
       </TabList>
       <Box top="46.5px" borderBottom="2px solid" mt="-2.5px" borderBottomColor="inherit" {...boxProps} />
 
