a644d64ece3557ea0872b9ced6c4f4f29ec83c3c
kahosan <15116355657l@gmail.com>
2025-09-11 12:58:18 +0800

    chore: improve vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import generouted from '@generouted/react-router/plugin';
 
@@ -6,35 +6,42 @@ import { version } from './package.json';
 process.env.VITE_APP_VERSION = version;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  build: {
-    rollupOptions: {
-      manualChunks: id => {
-        if (id.includes('node_modules')) return id.split('node_modules/')[1].split('/')[1];
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: './',
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: id => {
+            if (/(?!.*@chakra-ui).*(framer-motion|dplayer|hls.js|\breact-dom\b)/.test(id))
+              return id.split('node_modules/')[1].split('/')[1];
+          },
+        },
       },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8888',
-        changeOrigin: true,
-      },
-      '/bangumi': {
-        target: 'http://localhost:8888',
-        changeOrigin: true,
-      },
-      '/resource': {
-        target: 'http://localhost:8888',
-        changeOrigin: true,
+    server: {
+      proxy: {
+        '/api': {
+          target: env.API_URL || 'http://localhost:8888',
+          changeOrigin: true,
+        },
+        '/bangumi': {
+          target: env.API_URL || 'http://localhost:8888',
+          changeOrigin: true,
+        },
+        '/resource': {
+          target: env.API_URL || 'http://localhost:8888',
+          changeOrigin: true,
+        },
       },
     },
-  },
-  plugins: [react(), generouted()],
-  resolve: {
-    alias: {
-      '~': '/src',
+    plugins: [react(), generouted()],
+    resolve: {
+      alias: {
+        '~': '/src',
+      },
     },
-  },
+  };
 });
